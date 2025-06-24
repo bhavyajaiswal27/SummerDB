c97aa1d133583206faae074f5dd52b80ed0be4fe
Bhavya Jaiswal <jaiswalbhavya2005@gmail.com>
2025-06-24 15:27:57 +0530

    deep drowsiness model testing done

---
+++ cure_it/android/app/src/main/kotlin/com/example/cure_it/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cure_it
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ cure_it/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:cure_it/Pages/MainScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Mainscreen());
+}
+
